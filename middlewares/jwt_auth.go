e47e25de3cd3b9f1cd80da488697894f2dcfe261
Rock <1834309151@qq.com>
2022-02-06 22:55:25 +0800

    完成jwtToken验证中间件

--- middlewares/jwt_auth.go
+++ middlewares/jwt_auth.go
@@ -1,1 +1,41 @@
 package middlewares
+
+import (
+	"gin/dtos"
+	"gin/utils"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"net/http"
+)
+
+// JwtAuth JwtToken验证中间件
+func JwtAuth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.Request.Header.Get("Token")
+		if token == "" {
+			// 没有传token参数
+			c.JSON(http.StatusUnauthorized, dtos.ErrorDto{
+				Message:          "Requires key {Token} in headers",
+				DocumentationUrl: viper.GetString("Document.Url"),
+			})
+
+			c.Abort()
+			return
+		}
+
+		claims, err := utils.ParseJwtToken(token)
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, dtos.ErrorDto{
+				Message:          "Token expired or the other error occurred",
+				DocumentationUrl: viper.GetString("Document.Url"),
+			})
+
+			c.Abort()
+			return
+		}
+
+		// Claims写入上下文
+		c.Set("claims", claims)
+	}
+}
